ae09e3f2d77f7c9aa04dbe6fc482566529176c93
Kawsar Ahmed Fahad <kafahad1999@gmail.com>
2022-08-31 01:13:16 +0600

    solved nav basic issue

---
+++ src/components/Header.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import logo from '../assets/images/logo.svg';
+import close from '../assets/images/icon-close.svg';
+import hamburger from '../assets/images/icon-hamburger.svg';
+
+export const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <>
+      <nav>
+        <div className="mobile">
+          <img src={logo} alt="manage" className="logo" />
+
+          <div className="togglerIcon">
+            {isOpen ? (
+              <img src={close} alt="" onClick={toggle} />
+            ) : (
+              <img src={hamburger} alt="" onClick={toggle} />
+            )}
+          </div>
+        </div>
+
+        <div className={isOpen ? 'items ' : 'hide'}>
+          <a href="#">Pricing</a>
+          <a href="#">Product</a>
+          <a href="#">About us</a>
+          <a href="#">Careers</a>
+          <a href="#">Community</a>
+        </div>
+
+        <div className="ms-auto">
+          <button>Get Started</button>
+        </div>
+      </nav>
+    </>
+  );
+};
